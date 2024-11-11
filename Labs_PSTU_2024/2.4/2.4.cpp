5a54f1fbe491d798d2c419be9d85a0212b463408
upupupahah <100486353+upupupahah@users.noreply.github.com>
2024-11-11 22:49:41 +0500

    a

--- Labs_PSTU_2024/2.4/2.4.cpp
+++ Labs_PSTU_2024/2.4/2.4.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 int main()
-  {
-	  int N;
-	  std::cin >> N;
-	  for (int i = 0; i <= N; i++)
-	  {
-		  if (i % 2 != 0)
-		  {
-			  for (int q = 0; q <= (N - i) / 2; q++)
-			  {
-				  std::cout << " ";
-			  }
-			  for (int j = 0; j < i; j++)
-				  std::cout << "*";
-			  std::cout << std::endl;
-		  }
-	  }
-	  return 0;
-  }
+{
+	int N;
+	std::cin >> N;
+	if (N % 2 == 0 or N < 3)
+	{
+		std::cout << "error";
+		return 0;
+	}
+	for (int i = 0; i <= N; i++)
+	{
+		if (i % 2 != 0)
+		{
+			for (int q = 0; q <= (N - i) / 2; q++)
+			{
+				std::cout << " ";
+			}
+			for (int j = 0; j < i; j++)
+				std::cout << "*";
+			std::cout << std::endl;
+		}
+	}
+	return 0;
+}
