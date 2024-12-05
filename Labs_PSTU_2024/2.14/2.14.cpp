27c23b9253a81e5ad65d9b70cd2da5aaecdae68c
upupupahah <100486353+upupupahah@users.noreply.github.com>
2024-12-05 13:20:55 +0500

    05.12

---
+++ Labs_PSTU_2024/2.14/2.14.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+{
+	int N, sum = 0;
+	std::cin >> N;
+	while (N > 9)
+	{
+		sum += N % 10;
+		N /= 10;
+	}
+	sum += N;
+
+	std::cout << "summa = " << sum;
+	return 0;
+}
---
+++ Labs_PSTU_2024/2.19/2.19.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+int main()
+{
+	int N, sum = 0;
+	std::cin >> N;
+	for (int i = 1; i <= N; i++)
+	{
+		if (i % 3 == 0)
+		{
+			sum -= i;
+		}
+		else
+		{
+			sum += i;
+		}		
+	}
+	std::cout << sum;
+	return 0;
+}
